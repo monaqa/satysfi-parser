789ac750f61dfb0174b923935d6da12e037e2c71
monaqa <mogassy@yahoo.co.jp>
2021-03-19 09:21:41 +0900

    clear warnings

--- src/tests.rs
+++ src/tests.rs
@@ -1,13 +1,7 @@
 mod util;
-use crate::assert_parsed;
 use crate::grammar::satysfi_parser;
 use crate::types::Cst;
 
 use crate::types::rule::*;
 
 mod constant;
-
-#[macro_export]
-macro_rules! assert_parsed {
-    () => {};
-}
--- src/types.rs
+++ src/types.rs
@@ -29,17 +29,17 @@ pub mod rule {
         unit,
         boolean,
         int,
-        float,
-        length,
-        string,
+        // float,
+        // length,
+        // string,
     }
 }
 
 impl Cst {
-    /// 新たな CST を作成する。
-    pub fn new(rule: Rule, range: (usize, usize), inner: Vec<Cst>) -> Self {
-        Self { rule, range, inner }
-    }
+    // /// 新たな CST を作成する。
+    // pub fn new(rule: Rule, range: (usize, usize), inner: Vec<Cst>) -> Self {
+    //     Self { rule, range, inner }
+    // }
 
     /// Rule と range を指定して、葉に相当する（子のない） node を生成する。
     pub fn new_leaf(rule: Rule, range: (usize, usize)) -> Self {
@@ -50,14 +50,14 @@ impl Cst {
         }
     }
 
-    /// Rule と子を指定して、枝に相当する（子のある） node を生成する。
-    /// 省略した range は子の range の最小・最大から計算される。
-    pub fn new_node(rule: Rule, inner: Vec<Cst>) -> Self {
-        let range = inner.iter().fold((usize::MAX, 0), |acc, cst| {
-            let (acc_start, acc_end) = acc;
-            let (cst_start, cst_end) = cst.range;
-            (acc_start.min(cst_start), acc_end.max(cst_end))
-        });
-        Self { rule, range, inner }
-    }
+    // /// Rule と子を指定して、枝に相当する（子のある） node を生成する。
+    // /// 省略した range は子の range の最小・最大から計算される。
+    // pub fn new_node(rule: Rule, inner: Vec<Cst>) -> Self {
+    //     let range = inner.iter().fold((usize::MAX, 0), |acc, cst| {
+    //         let (acc_start, acc_end) = acc;
+    //         let (cst_start, cst_end) = cst.range;
+    //         (acc_start.min(cst_start), acc_end.max(cst_end))
+    //     });
+    //     Self { rule, range, inner }
+    // }
 }
