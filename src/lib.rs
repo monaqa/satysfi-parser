c982ff406acf42047cb148230c8649108c3e54ce
monaqa <mogassy@yahoo.co.jp>
2021-05-04 11:47:22 +0900

    simplify module path

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-pub mod grammar;
-pub mod types;
+mod grammar;
+mod types;
+
+pub use grammar::satysfi_parser::*;
+pub use types::rule::Rule;
+pub use types::{Cst, CstText};
 
 #[cfg(test)]
 mod tests;
